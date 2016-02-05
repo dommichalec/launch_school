ce1f197b440fe70c5525b6283dec04d51f8dbfc9
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-02-05 12:26:37 -0500

    add oop version of rps

---
+++ oo_programming/oo_rps/rps.rb
@@ -0,0 +1,165 @@
+# Rock Paper Scissor OOP style
+# RPS is a two person game where each player chooses one of
+# three possible moves: rock paper or scissors.
+
+# rock beats scissors
+# scissors beat paper
+# paper beats rock
+
+# if the players choose the same move, then it's a tie.
+
+# nouns: player, move, rule
+# Verbs: choose, compare
+
+# player
+# => choose
+# move
+# rule
+
+# => compare
+
+# Game Orchestration Engine
+class Game
+  attr_accessor(:human, :computer)
+
+  def initialize
+    @human = Human.new
+    @computer = Computer.new
+  end
+
+  def display_welcome_message
+    puts "Welcome to Rock, Paper, Scissors!"
+  end
+
+  def display_goodbye_message
+    puts "Thank for playing, #{human.name}! Good bye."
+  end
+
+  def display_winner
+    puts "You chose #{human.move}"
+    puts "#{computer.name} chose #{computer.move}"
+
+    if human.move > computer.move
+      puts "You win!"
+    elsif human.move < computer.move
+      puts "#{computer.name} won!"
+    else
+      puts "It's a tie!"
+    end
+  end
+
+  def play_again?
+    human_answer = nil
+    loop do
+      puts "do you want to play again? (y/n)"
+      human_answer = gets.chomp
+      break if ['y', 'n'].include?(human_answer.downcase)
+      puts "sorry, must choose either 'y' or 'n'."
+    end
+    human_answer.downcase == 'y' ? true : false
+  end
+
+  def play
+    display_welcome_message
+    loop do
+      human.choose
+      computer.choose
+      display_winner
+      break unless play_again?
+    end
+    display_goodbye_message
+  end
+end
+
+class Move
+  VALUES = %w(rock paper scissors)
+  def initialize(value)
+    @value = value
+  end
+
+  def scissors?
+    @value = 'scissors'
+  end
+
+  def paper?
+    @value = 'paper'
+  end
+
+  def rock?
+    @value = 'rock'
+  end
+
+  def to_s
+    @value
+  end
+
+  def >(other_move)
+    if rock?
+      return true if other_move.scissors?
+      return false
+    elsif paper?
+      return true if other_move.rock?
+      return false
+    else scissors?
+      return true if other_move.paper?
+      return false
+    end
+  end
+
+  def <(other_move)
+    if rock?
+      return true if other_move.paper?
+      return false
+    elsif paper?
+      return true if other_move.scissors?
+      return false
+    else scissors?
+      return true if other_move.rock?
+      return false
+    end
+  end
+end
+
+class Player
+  attr_accessor(:move, :name)
+  def initialize
+    set_name
+  end
+end
+
+class Human < Player
+  def set_name
+    n = ""
+    loop do
+      puts "what is your name?"
+      n = gets.chomp.capitalize
+      break unless n.empty?
+      puts "please input your name."
+    end
+    self.name = n
+    puts "nice to meet you #{self.name}!"
+  end
+
+  def choose
+    choice = nil
+    loop do
+      puts "please choose rock paper or scissors"
+      choice = gets.chomp
+      break if Move::VALUES.include?(choice)
+      puts "sorry, invalid choice."
+    end
+    self.move = Move.new(choice)
+  end
+end
+
+class Computer < Player
+  def set_name
+    self.name = %w(Helmsworth Brody Sebastian McCaffrey).sample
+  end
+
+  def choose
+    self.move = Move.new(Move::VALUES.sample)
+  end
+end
+
+Game.new.play
