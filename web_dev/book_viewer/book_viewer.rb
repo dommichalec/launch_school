1506b67bfe193b74759facc67003d005d1540713
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-03-15 13:24:00 -0400

    add helpers and search

--- web_dev/book_viewer/book_viewer.rb
+++ web_dev/book_viewer/book_viewer.rb
@@ -1,20 +1,47 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
+require_relative 'helpers'
+
+before do
+  @table_of_contents = File.readlines('data/toc.txt')
+end
 
 get '/read' do
   @title = 'The Adventures of Sherlock Holmes'
   @author = 'Sir Arthur Doyle'
-  @table_of_contents = File.readlines('data/toc.txt')
 
   erb :home
 end
 
-get '/chapters/1' do
-  @title = "Chapter 1"
-  @author = 'Sir Author Doyle'
-  @table_of_contents = File.readlines("data/toc.txt")
-  @chapter = File.read("data/chp1.txt")
+get '/chapters/:number' do
+  number = params[:number].to_i
+  chapter_name = @table_of_contents[number - 1]
+  @title = "Chapter #{number}: #{chapter_name}"
 
-  erb :chapters
+  @chapter = File.read("data/chp#{number}.txt")
+
+  erb :chapter
+end
+
+not_found do
+  redirect '/read'
+end
+
+get '/search' do
+  if params[:query]
+    # each result in this array will be:
+    # [ chapter name, chapter index, paragraph index ]
+    @results = @table_of_contents.each_with_index.each_with_object([]) do |(chapter, index), results|
+      text = File.read("data/chp#{index + 1}.txt")
+      paragraphs = text.split("\n\n")
+      paragraphs.each_with_index do |paragraph, paragraph_index|
+        if paragraph.include?(params[:query])
+          results << [chapter, index, paragraph, paragraph_index]
+        end
+      end
+    end
+  end
+
+  erb :search
 end
